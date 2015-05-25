dede434c5b2ce7bee7f678609a49156a4dbfcd64
Jenny L <jenny.lipa@miraid3sign.comjenny.lipa@miraid3sign.com>
2015-05-25 10:09:49 +0800

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Jenny L on 5/25/15.
+//  Copyright (c) 2015 Mirai D3sign. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Jenny L on 5/25/15.
+//  Copyright (c) 2015 Mirai D3sign. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

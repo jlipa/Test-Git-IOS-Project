a7335a4f4f3f99c1dcc7b1290065998122214d89
Jenny L <jenny.lipa@miraid3sign.comjenny.lipa@miraid3sign.com>
2015-05-25 11:06:37 +0800

    Added a test class

---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Jenny L on 5/25/15.
+//  Copyright (c) 2015 Mirai D3sign. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
